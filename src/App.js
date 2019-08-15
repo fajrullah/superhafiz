ce0678426475fbd5ef2917b2ca25fee002b96343
diah06 <diahputrilestari6@gmail.com>
2019-08-15 15:24:42 +0700

    touchdown

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-
-function App() {
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
+import Home from './Home';
+import About from './About';
+import Features from './Features';
+import Talk from './Talk';
+import CountingDown from './CountingDown';
+import Contact from './Contact';
+import img from './images/header-background.jpg';
+import ParticlesPage from './ParticlesPage';
+class App extends Component {
+  render(){ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+  <div className="backgroundImage">
+    <ParticlesPage></ParticlesPage>
+    <AliceCarousel
+      duration={400}
+      mouseDragEnabled={true}
+      infinite={true}
+      keyboardNavigation={true}
+      fadeOutAnimation={true}
+      buttonsDisabled={true}>
+      <div className="item" id="Home"><Home/></div>
+      <div className="item" id="About"><About/></div>
+      <div className="item" id="Features"><Features/></div>
+      <div className="item" id="Talk"><Talk/></div>
+      <div className="item" id="CountingDown"><CountingDown/></div>
+      <div className="item" id="Contact"><Contact/></div>
+    </AliceCarousel>
+  </div>
   );
+  }
 }
-
 export default App;
