5eef01f63e8185d6c888eef85c928ac00edabe76
diah06 <diahputrilestari6@gmail.com>
2019-08-27 11:56:42 +0700

    still in progress

--- src/About.js
+++ src/About.js
@@ -1,40 +1,47 @@
 import React, { Component } from 'react';
 import './App.css';
-import HAFIZAI from './images/HAFIZAI.png';
+// import HAFIZAI from './images/HAFIZAI.png';
 // import circle from './images/circle.png';
+import hafiz from './images/hafiz.png';
+import baling2 from './images/baling2.png';
+import center_baling2 from './images/center.png';
+import planet_orange from './images/planet-orange.png';
+import planet_ungu from './images/planet-ungu.png';
+import awan from './images/awan.png';
+import bg_awan from './images/bg_cloud.png';
 import {  Row, Col } from 'react-bootstrap';
 
 class About extends Component{
   render() {
     return (
     	<div className="container">
-			<h1 className="App title_all_page">Kenalan sama Hafiz</h1>
     		<Row>
     			<Col>
-    				<div>
-    					{/*<img src={circle} alt="circle" style={{width:30+'%', marginTop:50+'px', marginLeft:50+'px'}} className="image-1"/>*/}
-    				</div>
-    				<div>
-    					{/*<img src={circle} style={{width:30+'%', marginTop:50+'px', marginLeft:50+'px'}} alt="circle" className="image-1"/>*/}
-    				</div>	
+					<h3 className="hafiz_desc_tittle">SuperHafiz about</h3>
+					<h4 className="hafiz_desc">
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+						Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+						when an unknown printer took a galley of type and scrambled it to make a type specimen book
+						It has survived not only five centuries, but also the leap into electronic typesetting,
+						remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+						and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</h4>
+					<img src={awan} className="awan_about_kiri" alt="cloud in page about"/>
 			    </Col>
 			    <Col>
-			    	<div className="App">
-			    		<img src={HAFIZAI} style={{width:60+'%', marginTop:10+'px'}} alt="slide 2"/>
-			    	</div>
-			    	<div>
-    					{/*<img src={circle} style={{width:30+'%', marginTop:50+'px', marginLeft:60+'px'}} alt="circle" className="image-1"/>*/}
-    				</div>
+					<img src={hafiz} className="hafiz_about" alt="hafiz"/>
+					<img src={baling2} className="hafiz_baling_about" alt="baling baling hafiz"/>
+					<img src={center_baling2} className="center_baling2_about" alt="center baling baling hafiz"/>
 			    </Col>
 			    <Col>
-			    	<div>
-    					{/*<img src={circle} style={{width:30+'%', marginTop:10+'px', marginLeft:120+'px'}} alt="circle" className="image-1"/>*/}
-    				</div>
-    				<div>
-    					{/*<img src={circle} style={{width:30+'%', marginTop:50+'px', marginRight:70+'px'}} alt="circle" className="image-1"/>*/}
-    				</div>	
+					<img src={planet_orange} className="planet_orange_about" alt="orange planet in page about"/>
+					<img src={planet_ungu} className="planet_ungu_about" alt="purple planet in page about"/>
+					<img src={awan} className="awan_about_kanan" alt="cloud in page about"/>
 			    </Col>
 		    </Row>
+			<Row className="row_bg_awan">
+				<img src={bg_awan} className="bg_awan" alt="cloud in page about"/>
+			</Row>
     	</div>
     );
   }
--- src/Home.js
+++ src/Home.js
@@ -2,33 +2,39 @@ import React, { Component } from 'react';
 import './App.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import awan from './images/awan.png';
-import planet_kiri from './images/planet-kiri.png';
-import planet_kanan from './images/planet-kanan.png';
+import planet_orange_home from './images/planet-orange.png';
+import planet_ungu_home from './images/planet-ungu.png';
 import hafiz from './images/hafiz.png';
-// import baling_baling from './images/baling.png';
 import baling2 from './images/baling2.png';
 import center from './images/center.png';
+// import bumi from './images/bumi-muter.png';
+// import bumi_layout from './images/bumi-layout.png';
+// import awan3d from './images/cloud.png';
 
 class Home extends Component{
   render() {
     return (
       <div>
-        <Container className="container">
-            <Row style={{height:300+'px'}}>
-              <Col>
-                <img src={planet_kiri} className="planet_kiri" alt="planet kiri"/>
+        <Container style={{height:100+'%',width:100+'%'}}>
+            <Row style={{height:100+'%',width:100+'%'}}>
+              <Col style={{width:100+'%',height:100+'%'}}>
+                <img src={planet_orange_home} className="planet_orange_home" alt="planet orange home"/>
                 <img src={awan} className="awan_front" alt="awan"/>
-             </Col>
-              <Col>
-                <img src={awan} className="awan_back_atas" alt="awan"/>
+              </Col>
+              <Col style={{height: 100+'%'}}>
+                {/* <img src={awan3d} className="awan_back_atas" alt="awan"/> */}
                 <img src={hafiz} className="hafiz_home" alt="hafiz"/>
                 <img src={baling2} className="hafiz_baling_baling" alt="baling baling hafiz"/>
                 <img src={center} className="center_baling" alt="baling baling hafiz"/>
               </Col>
               <Col>
-                <img src={planet_kanan} className="planet_kanan" alt="planet kanan"/>
+                <img src={planet_ungu_home} className="planet_ungu_home" alt="planet kanan"/>
                 <img src={awan} className="awan_back" alt="awan"/>
               </Col>
+            </Row>
+            <Row className="row_bumi">
+              {/* <img src={bumi_layout} className="bumi_muter" alt="awan"/> */}
+              <div className="bumi_muter"></div>
             </Row>
         </Container>
       </div>
